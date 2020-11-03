08fd3186b871247348094ebbd5a2f8e7fd9c1f90
SudoAdam <adam5530@stud.kea.dk>
2020-11-03 13:24:08 +0100

    loging related stuff

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -3,11 +3,36 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/logIn")
+    public String logIn(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model) {
+        String retur = "";
+        return retur;
+    }
+
+
+    @PostMapping("/createUser")
+    public String createUser(
+            @RequestParam String name,
+            @RequestParam String password,
+            @RequestParam int phoneNum,
+            @RequestParam String street,
+            @RequestParam int streetNum,
+            @RequestParam int zipcode,
+            Model model) {
+        return "confirmation";
     }
 }
--- src/main/java/com/example/demo/user/LogIn.java
+++ src/main/java/com/example/demo/user/LogIn.java
@@ -1,4 +1,11 @@
 package com.example.demo.user;
 
 public class LogIn {
+
+    public boolean userExsist (String user, String password){
+        Boolean exsist = false;
+
+
+        return exsist;
+    }
 }
