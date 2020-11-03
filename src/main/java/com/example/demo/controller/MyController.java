e26d377319ee8579b16428198f0cbf96ca6391f3
SudoAdam <adam5530@stud.kea.dk>
2020-11-03 15:43:53 +0100

    login migth work now... without database

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -23,25 +23,28 @@ public class MyController {
 
 
     //hugget fra gammel projekt !på ingen måde færdigt!
-    @PostMapping("/logIn")
+    @GetMapping("/logIn")
     public String logIn(
             @RequestParam String username,
             @RequestParam String password,
-            @RequestParam String action) {
+            @RequestParam int action,
+            Model model) {
         //forsøg på at få index formen til både at kunne logge ind og oprette
-        if (action == "login") {
+        System.out.println("Så langt så godt");
+        if (action == 1) {
             System.out.println("login tried");
-            logIn.login(username, password);
-        } else if (action == "create") {
+            //logIn.login(username, password);
+        } else if (action == 2) {
             System.out.println("create tried");
-            logIn.create(username, password);
+            //logIn.create(username, password);
         } else {
             System.out.println("der er gået noget galt");
         }
+
         return "omMig";
 
     }
-
+/*
     //hugget fra gammel projekt !på ingen måde færdigt!
     @PostMapping("/createUser")
     public String createUser(
@@ -49,5 +52,5 @@ public class MyController {
             @RequestParam String password,
             Model model) {
         return "confirmation";
-    }
+    }*/
 }
