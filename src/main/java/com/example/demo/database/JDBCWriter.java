ba14095bace4b271e0ced224fb85018ccaf9ce00
SudoAdam <adam5530@stud.kea.dk>
2020-11-03 13:33:49 +0100

    JDBC and other database related added

---
+++ src/main/java/com/example/demo/database/JDBCWriter.java
@@ -0,0 +1,113 @@
+package com.example.demo.database;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class JDBCWriter {
+
+    private Connection connection = null;
+
+    public boolean setConnection() {
+        final String url = "jdbc:mysql://localhost:3306/urlread?serverTimezone=UTC";
+        boolean bres = false;
+        try {
+            connection = DriverManager.getConnection(url, "user", "User");
+            bres = true;
+        } catch (SQLException ioerr) {
+            System.out.println("Vi fik IKKE connection=" + ioerr.getMessage());
+        }
+
+        return bres;
+    }
+
+    public Vector<String> getLines(String aURL, String aWord) {
+        String seachStr = "SELECT left(line,50) as line FROM urlreads where url like ? and line like ? LIMIT 20";
+        PreparedStatement preparedStatement;
+        Vector<String> v1 = new Vector<>();
+        try {
+            preparedStatement = connection.prepareStatement(seachStr);
+            preparedStatement.setString(1, "%" + aURL + "%");
+            preparedStatement.setString(2, "%" + aWord + "%");
+            System.out.println(seachStr);
+            ResultSet resset = preparedStatement.executeQuery();
+            while (resset.next()) {
+                String str1 = "" + resset.getObject("line");
+                v1.add(str1);
+            }
+        } catch (SQLException sqlerr) {
+            System.out.println("Error in select = " + sqlerr.getMessage());
+        }
+        return v1;
+    }
+
+    public int searchDB(String aUrl, String aWord) {
+        String searchStr = "SELECT count(*) as line FROM urlreads where url like ? and line like ?";
+        PreparedStatement preparedStatement;
+        int res = -1;
+        try {
+            preparedStatement = connection.prepareStatement(searchStr);
+            preparedStatement.setString(1, "%" + aUrl + "%");
+            preparedStatement.setString(2, "%" + aWord + "%");
+            System.out.println(searchStr);
+            ResultSet resset = preparedStatement.executeQuery();
+            if (resset.next()) {
+                String str = "" + resset.getObject(1);
+                res = Integer.parseInt(str);
+                System.out.println("fundet antal = " + res);
+            }
+        } catch (SQLException sqlerr) {
+            System.out.println("fejl i søgning = " + sqlerr.getMessage());
+        }
+        return res;
+    }
+
+    public int deleteRows(String aURL, String aWord) {
+        String delStr = "DELETE FROM urlreads where url like ? and line like ?";
+        PreparedStatement preparedStatement;
+        int res = -1;
+        try {
+            preparedStatement = connection.prepareStatement(delStr);
+            preparedStatement.setString(1, "%" + aURL + "%");
+            preparedStatement.setString(2, "%" + aWord + "%");
+            res = preparedStatement.executeUpdate();
+        } catch (SQLException sqlerr) {
+            System.out.println("Error in delete =" + sqlerr.getMessage());
+        }
+        return res;
+    }
+
+    public int writeLines(String aUlr, ArrayList<String> aLst) {
+        String insstr = "INSERT INTO urlreads(url, line, linelen, medtext) values (?, ?, ?, ?)";
+        PreparedStatement preparedStatement;
+        int res = 0;
+        String lineln = "";
+        for (String line : aLst) {
+            try {
+                preparedStatement = connection.prepareStatement(insstr);
+                preparedStatement.setString(1, aUlr);
+                lineln = "" + line.length();
+                if (line.length() < 15000) {
+                    preparedStatement.setString(2, line);
+                    preparedStatement.setString(3, "" + lineln);
+                    preparedStatement.setString(4, " ");
+                } else {
+                    preparedStatement.setString(2, " ");
+                    preparedStatement.setString(3, "" + lineln);
+                    preparedStatement.setString(4, line);
+                }
+                int rowcount = preparedStatement.executeUpdate();
+                //System.out.println("Indsat række=" + rowcount);
+                res = res + rowcount;
+                if (res % 100 == 0) {
+                    System.out.println("Har saved rowcount=" + res);
+                }
+            } catch (SQLException sqlerr) {
+                String errmsg = sqlerr.getMessage();
+                System.out.println("Fejl i INSERT=" + errmsg + " len=" + lineln);
+            }
+        }
+        return res;
+    }
+
+}
