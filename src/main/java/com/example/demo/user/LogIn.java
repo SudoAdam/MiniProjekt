b9e471459974dad60a026fe34b8cea1925f0f6a2
SudoAdam <adam5530@stud.kea.dk>
2020-11-04 11:13:20 +0100

    password in login form changed

--- src/main/java/com/example/demo/user/LogIn.java
+++ src/main/java/com/example/demo/user/LogIn.java
@@ -2,13 +2,15 @@ package com.example.demo.user;
 
 public class LogIn {
 
+
     public boolean userExsist (String user){
         Boolean exsist = false;
         return exsist;
     }
 
-    public void login (String username, String password){
-
+    public User login (String username, String password){
+      User user = new User(1, username, password);
+      return user;
     }
     public void create (String username, String password){
 
